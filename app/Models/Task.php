3178eab8b35821e41c3082ae9b905e967881db8a
Raisa P Silva <raisa.gagliano@gmail.com>
2026-08-04 10:16:43 -0300

    feat: configura relacionamentos nos models e cria controllers base

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'project_id',
+        'title',
+        'description',
+        'status',
+    ];
+
+    public function project(): BelongsTo // 1:1
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
